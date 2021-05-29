6b30f240bf2079bcb4a4fe2b815745edf803174a
Saurabh_M <Saurabh_M@SAURABH-PC>
2021-05-29 12:49:26 +0530

    by saurabh

---
+++ DemoJenkins1packageLevel/src/DemoJ1/JenkinsDemo1.java
@@ -0,0 +1,21 @@
+package DemoJ1;
+
+public class JenkinsDemo1 {
+
+
+	public static void main(String[] args) {
+		int[]x;
+		x=new int[5];
+		x[0]=10;
+		x[1]=20;
+		x[2]=30;
+		x[3]=40;
+		x[4]=50;
+
+		int []y=new int[] {15,25,35,45,55};
+		double []z= {1.5,1.6,1.7,1.8,1.9};
+		System.out.println(x[0]);
+		System.out.println(y[0]);
+		System.out.println(z[0]);
+	}
+}
